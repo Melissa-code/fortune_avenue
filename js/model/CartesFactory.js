240329e7ad7bd4b8172b0de8f8e534fcde04fe10
Melissa Benard <benardmeli@yahoo.fr>
2025-11-06 22:22:44 +0100

    feat: add data cartes chances and fonds communs in json files

---
+++ js/model/CartesFactory.js
@@ -0,0 +1,22 @@
+class CartesFactory {
+
+    static createCartesChance() {
+        let cartesChanceList = []; 
+     
+        return cartesChanceList;
+    }
+
+    static createCartesFondsCommun() {
+        let cartesFondsCommunList = []; 
+        
+        return cartesFondsCommunList
+    }
+
+    static createCartesRue() {
+        let cartesRuesList = []; 
+
+        return cartesRuesList; 
+    }
+}
+
+export default CartesFactory; 
