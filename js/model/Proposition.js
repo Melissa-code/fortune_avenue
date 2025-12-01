054474ce859844e733071595afe158b75fd6810b
Melissa Benard <benardmeli@yahoo.fr>
2025-12-01 21:09:40 +0100

    feat: complete Proposition with children classes

--- js/model/Proposition.js
+++ js/model/Proposition.js
@@ -1,3 +1,5 @@
+import { CasePropriete } from "./CaseJeu.js";
+
 export class Proposition {
     constructor(titre, description) {
         this.titre = titre;
@@ -13,25 +15,116 @@ export class Proposition {
     }
 
     getListePropositions() {
-        //
+        return [
+            new PropositionAcheterPropriete(),
+            new PropositionHypothequer(),
+            new PropositionLeverHypotheque(),
+            new PropositionConstruireMaison(),
+            new PropositionConstruireHotel(),
+            new PropositionPayerLoyer()
+        ];
     }
-
 }
+
 
 export class PropositionAcheterPropriete extends Proposition{
     constructor(titre, description) {
         super(titre, description);
     }
 
-    estDisponible(jeu, joueur, caseJeu) {
-        if (caseJeu.proprietaire === null && joueur.argent >= caseJeu.prix) {
+    estDisponible(jeu, joueur, casePropriete) {
+        if (casePropriete.proprietaire === null && joueur.argent >= casePropriete.prixAchat) {
             return true; 
         }
         return false; 
     }
 
-    valider(jeu, joueur, caseJeu) {
+    valider(jeu, joueur, casePropriete) {
+        joueur.argent -= casePropriete.prixAchat; 
+        casePropriete.proprietaire = joueur; 
+    }
+}
+
+
+export class PropositionHypothequer extends Proposition {
+    constructor(titre, description) {
+        super(titre, description);
+    }
+
+    estDisponible(jeu, joueur, casePropriete) {
+        
+    }
+
+    valider(jeu, joueur, casePropriete) {
         
     }
 }
 
+
+export class PropositionLeverHypotheque extends Proposition{
+    constructor(titre, description) {
+        super(titre, description);
+    }
+
+    estDisponible(jeu, joueur, casePropriete) {
+        
+    }
+
+    valider(jeu, joueur, casePropriete) {
+        
+    }
+}
+
+
+export class PropositionConstruireMaison extends Proposition{
+    constructor(titre, description) {
+        super(titre, description);
+    }
+
+    estDisponible(jeu, joueur, caseRue) {
+        if (caseRue.proprietaire === joueur && caseRue.possederTouteLaCollection() && caseRue.nombreMaisons < 4 && joueur.argent >= caseRue.prixMaison) {
+            return true;
+        }
+        return false; 
+    }
+
+    valider(jeu, joueur, caseRue) {
+        joueur.argent -= caseRue.prixMaison; 
+        caseRue.nombreMaisons++; 
+    }
+}
+
+
+export class PropositionConctruireHotel extends Proposition {
+    constructor(titre, description) {
+        super(titre, description);
+    }
+
+    estDisponible(jeu, joueur, caseRue) {
+        if (caseRue.proprietaire === joueur && caseRue.nombreMaisons === 4 && joueur.argent >= caseRue.prixHotel) {
+            return true;
+        }
+        return false; 
+    }
+
+    valider(jeu, joueur, caseRue) {
+        joueur.argent -= caseRue.priHotel; 
+        caseRue.nombreMaisons = 0;
+        caseRue.nombreHotels = 1; //1 hotel 
+    }
+}
+
+
+export class PropositionPayerLoyer extends Proposition {
+    constructor(titre, description) {
+        super(titre, description);
+    }
+
+    estDisponible(jeu, joueur, casePropriete) {
+        
+    }
+
+    valider(jeu, joueur, casePropriete) {
+        
+    }
+}
