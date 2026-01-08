916ce678c57edbbc1de52b88b9b721c48fd09fc4
Melissa-code <benardmeli@yahoo.fr>
2026-01-08 16:01:26 +0100

    feat: add enum TypesEffets for cards

---
+++ js/model/enums/TypesEffets.js
@@ -0,0 +1,9 @@
+const TypesEffets = Object.freeze({
+    DEPLACEMENT: "deplacement",
+    VERSEMENT: "versement",
+    PIOCHER: "piocher",
+    PRISON: "prison",
+    REPARATIONS: "reparations"
+})
+
+export default TypesEffets;
