4a7b35969959b2187274a8ad1063cff3ff6c9095
Melissa Benard <benardmeli@yahoo.fr>
2026-07-25 19:35:23 +0200

    test: add tests for CarteEffetsFactory

--- js/model/CarteEffetsFactory.js
+++ js/model/CarteEffetsFactory.js
@@ -1,126 +1,129 @@
 import TypesEffets from "./enums/TypesEffets.js";
 import { CarteAction } from "./Carte.js";
-import { DeplacementEffet, VersementEffet, GareProcheEffet, ReparationsEffet, PrisonEffet, ChoixEffet } from "./Effet.js";
-
+import {
+  DeplacementEffet,
+  VersementEffet,
+  GareProcheEffet,
+  ReparationsEffet,
+  PrisonEffet,
+  ChoixEffet,
+} from "./Effet.js";
 
 export class CarteEffetsFactory {
-    /**
-     * parcourt les données de data/effets_chance.js ou data/effets_fonds_communs.js
-     * @return {Array} de cartes
-     */
-    static chargerDataEffetsCartes(effetsCartesJson) {
-        const cartes = [];
-        
-        for (const effetCarteJson of effetsCartesJson) {
-            const carte = CarteEffetsFactory.generateCarte(effetCarteJson);
-            cartes.push(carte);
-        }
-        
-        return cartes;
+  /**
+   * parcourt les données de data/effets_chance.js/data/effets_fonds_communs.js
+   * @return {Array} de cartes
+   */
+  static chargerDataEffetsCartes(effetsCartesJson) {
+    const cartes = [];
+
+    for (const effetCarteJson of effetsCartesJson) {
+      const carte = CarteEffetsFactory.generateCarte(effetCarteJson);
+      cartes.push(carte);
     }
 
-    static generateCarte(jsonObj) {
-        switch (jsonObj.type) {
-            case TypesEffets.DEPLACEMENT:
-                return CarteEffetsFactory.parseCarteDeplacement(jsonObj);
-            case TypesEffets.GARE_PROCHE: 
-                return CarteEffetsFactory.parseCarteGareProche(jsonObj);
-            case TypesEffets.VERSEMENT:
-                return CarteEffetsFactory.parseCarteVersement(jsonObj);   
-            case TypesEffets.REPARATIONS:
-                return CarteEffetsFactory.parseCarteReparations(jsonObj);
-            case TypesEffets.ALLER_EN_PRISON:
-                return CarteEffetsFactory.parseCarteAllerEnPrison(jsonObj);
-            case TypesEffets.SORTIR_DE_PRISON:
-                return CarteEffetsFactory.parseCarteSortirDePrison(jsonObj);
-            case TypesEffets.CHOIX: 
-                return CarteEffetsFactory.parseCarteChoix(jsonObj);
-            default:
-                console.error(`Type de carte chance inconnu: ${jsonObj.type}`);
-                return null;
-        }       
+    return cartes;
+  }
+
+  static generateCarte(jsonObj) {
+    switch (jsonObj.type) {
+      case TypesEffets.DEPLACEMENT:
+        return CarteEffetsFactory.parseCarteDeplacement(jsonObj);
+      case TypesEffets.GARE_PROCHE:
+        return CarteEffetsFactory.parseCarteGareProche(jsonObj);
+      case TypesEffets.VERSEMENT:
+        return CarteEffetsFactory.parseCarteVersement(jsonObj);
+      case TypesEffets.REPARATIONS:
+        return CarteEffetsFactory.parseCarteReparations(jsonObj);
+      case TypesEffets.ALLER_EN_PRISON:
+        return CarteEffetsFactory.parseCarteAllerEnPrison(jsonObj);
+      case TypesEffets.SORTIR_DE_PRISON:
+        return CarteEffetsFactory.parseCarteSortirDePrison(jsonObj);
+      case TypesEffets.CHOIX:
+        return CarteEffetsFactory.parseCarteChoix(jsonObj);
+      default:
+        console.log(`Type de carte chance inconnu: ${jsonObj.type}`);
+        return null;
+    }
+  }
+
+  static parseCarteDeplacement(jsonObj) {
+    const valeur_deplacement =
+      jsonObj.type_deplacement === "absolu"
+        ? jsonObj.index_case
+        : jsonObj.nombreDePas;
+
+    return new CarteAction(jsonObj.titre, jsonObj.description, [
+      new DeplacementEffet(
+        jsonObj.type_deplacement,
+        valeur_deplacement,
+        jsonObj.bonusPassage,
+      ),
+    ]);
+  }
+
+  static parseCarteGareProche(jsonObj) {
+    return new CarteAction(jsonObj.titre, jsonObj.description, [
+      new GareProcheEffet(
+        jsonObj.type_deplacement,
+        jsonObj.nombreDePas,
+        jsonObj.bonusPassage,
+      ),
+    ]);
+  }
+
+  static parseCarteVersement(jsonObj) {
+    const estCollectif = jsonObj.titre === "Fonds communs 9";
+
+    return new CarteAction(jsonObj.titre, jsonObj.description, [
+      new VersementEffet(
+        jsonObj.montant,
+        jsonObj.source,
+        jsonObj.destinataire,
+        estCollectif,
+      ),
+    ]);
+  }
+
+  static parseCarteAllerEnPrison(jsonObj) {
+    return new CarteAction(jsonObj.titre, jsonObj.description, [
+      new PrisonEffet(true),
+    ]);
+  }
+
+  static parseCarteSortirDePrison(jsonObj) {
+    return new CarteAction(jsonObj.titre, jsonObj.description, [
+      new PrisonEffet(false),
+    ]);
+  }
+
+  static parseCarteReparations(jsonObj) {
+    return new CarteAction(jsonObj.titre, jsonObj.description, [
+      new ReparationsEffet(
+        jsonObj.montant_par_maison,
+        jsonObj.montant_par_hotel,
+        jsonObj.source,
+        jsonObj.destinataire,
+      ),
+    ]);
+  }
+
+  // choix entre piocher carte "chance" ou versement d'une amende (ex: Fonds communs 9)
+  static parseCarteChoix(jsonObj) {
+    return new CarteAction(jsonObj.titre, jsonObj.description, [
+      new ChoixEffet(jsonObj.montant, jsonObj.source, jsonObj.destinataire),
+    ]);
+  }
+
+  static melangerCartes(effetsCartesJson) {
+    const pioche = effetsCartesJson;
+
+    // mélange la pioche (Fisher-Yates)
+    for (let i = pioche.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); //nb de 0 à i
+      [pioche[i], pioche[j]] = [pioche[j], pioche[i]];
     }
 
-    static parseCarteDeplacement(jsonObj) {
-        const valeur_deplacement = jsonObj.type_deplacement === "absolu" ? jsonObj.index_case : jsonObj.nombreDePas;
-        
-        return new CarteAction(
-            jsonObj.titre,
-            jsonObj.description,    
-            [new DeplacementEffet(jsonObj.type_deplacement, valeur_deplacement, jsonObj.bonusPassage)]
-        );    
-    }
-
-    static parseCarteGareProche(jsonObj) {
-        return new CarteAction(
-            jsonObj.titre,
-            jsonObj.description,    
-            [new GareProcheEffet(jsonObj.type_deplacement, jsonObj.nombreDePas, jsonObj.bonusPassage)]
-        );    
-    }
-
-    static parseCarteVersement(jsonObj) {
-        const estCollectif = jsonObj.titre === "Fonds communs 9"; 
-        
-        return new CarteAction(
-            jsonObj.titre,
-            jsonObj.description,    
-            [new VersementEffet(
-                jsonObj.montant, 
-                jsonObj.source, 
-                jsonObj.destinataire, 
-                estCollectif)
-            ],
-        );    
-    }   
-
-    static parseCarteAllerEnPrison(jsonObj) {
-        return new CarteAction(
-            jsonObj.titre,
-            jsonObj.description,    
-            [new PrisonEffet(true)]
-        );    
-    }   
-
-    static parseCarteReparations(jsonObj) {             
-        return new CarteAction(
-            jsonObj.titre,
-            jsonObj.description,      
-            [new ReparationsEffet(
-                jsonObj.montant_par_maison, 
-                jsonObj.montant_par_hotel, 
-                jsonObj.source, 
-                jsonObj.destinataire
-            )],
-        );    
-    }
-
-    static parseCarteSortirDePrison(jsonObj) {             
-        return new CarteAction(
-            jsonObj.titre,
-            jsonObj.description,    
-            [new PrisonEffet(false)]
-        );    
-    }
-
-    // choix entre piocher carte "chance" ou versement d'une amende (ex: Fonds communs 9)
-    static parseCarteChoix(jsonObj) {
-        return new CarteAction(
-            jsonObj.titre,
-            jsonObj.description,    
-            [new ChoixEffet(jsonObj.montant, jsonObj.source, jsonObj.destinataire)]
-        ); 
-    }
-
-    static melangerCartes(effetsCartesJson) {
-        const pioche = effetsCartesJson; 
-
-        // mélange la pioche (Fisher-Yates)
-        for (let i = pioche.length -1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1)); //nb de 0 à i
-            [pioche[i], pioche[j]] = [pioche[j], pioche[i]]; 
-        }
-
-        return pioche; 
-    }
+    return pioche;
+  }
 }
--- tests/CarteEffetsFactory.test.js
+++ tests/CarteEffetsFactory.test.js
@@ -1,63 +1,235 @@
-import { CarteEffetsFactory } from '../js/model/CarteEffetsFactory.js';
+import { jest } from "@jest/globals";
+import { CarteEffetsFactory } from "../js/model/CarteEffetsFactory.js";
 import TypesEffets from "../js/model/enums/TypesEffets.js";
 import { CarteAction } from "../js/model/Carte.js";
-import { DeplacementEffet, VersementEffet, GareProcheEffet, ReparationsEffet, PrisonEffet, ChoixEffet } from "../js/model/Effet.js";
+import {
+  DeplacementEffet,
+  VersementEffet,
+  GareProcheEffet,
+  ReparationsEffet,
+  PrisonEffet,
+  ChoixEffet,
+} from "../js/model/Effet.js";
 
-describe('CarteEffetsFactory', () => {
-    // tests charger DataEffetsCartes
-    test('chargerDataEffetsCartes() retourne un tableau de cartes', () => {
-        const effetsCartesJson = [
-            {
-                "titre": "Chance 1",
-                "description": "Allez sur la case départ.",
-                "type": TypesEffets.DEPLACEMENT,
-                "type_deplacement": "absolu",
-                "index_case": 5,
-                "nombreDePas": 0,
-                "bonusPassage": 200,
-            },
-            {
-                "titre": "Chance 2",
-                "description": "Versez 100 M à la banque.",
-                "type": TypesEffets.VERSEMENT,
-                "montant": 100,
-                "source": "banque",
-                "destinataire": "joueur",
-            },
-        ];
+describe("CarteEffetsFactory", () => {
+  // tests charger DataEffetsCartes
+  test("chargerDataEffetsCartes() retourne un tableau de cartes", () => {
+    const effetsCartesJson = [
+      {
+        titre: "Chance 1",
+        description: "Allez sur la case départ.",
+        type: TypesEffets.DEPLACEMENT,
+        type_deplacement: "absolu",
+        index_case: 5,
+        nombreDePas: 0,
+        bonusPassage: 200,
+      },
+      {
+        titre: "Chance 2",
+        description: "Versez 100 M à la banque.",
+        type: TypesEffets.VERSEMENT,
+        montant: 100,
+        source: "banque",
+        destinataire: "joueur",
+      },
+    ];
 
-        const cartes = CarteEffetsFactory.chargerDataEffetsCartes(effetsCartesJson);
+    const cartes = CarteEffetsFactory.chargerDataEffetsCartes(effetsCartesJson);
 
-        expect(cartes).toHaveLength(2);
-        expect(cartes[0]).toBeInstanceOf(CarteAction);
-        expect(cartes[1]).toBeInstanceOf(CarteAction);
-    });
+    expect(cartes).toHaveLength(2);
+    expect(cartes[0]).toBeInstanceOf(CarteAction);
+    expect(cartes[1]).toBeInstanceOf(CarteAction);
+  });
 
-    // tests sur generateCarte()
-    test('appelle le bon parseur selon le type', () => {
-        const effetsCartesJson = {
-            type: TypesEffets.DEPLACEMENT,
-            titre: 'Chance 1',
-            description: 'test',
-            type_deplacement: 'absolu',
-            index_case: 5
-        };
+  // tests sur generateCarte()
+  test("appelle le bon parseur selon le type", () => {
+    const effetsCartesJson = {
+      type: TypesEffets.DEPLACEMENT,
+      titre: "Chance 1",
+      description: "test",
+      type_deplacement: "absolu",
+      index_case: 5,
+    };
 
-        const carte = CarteEffetsFactory.generateCarte(effetsCartesJson);
+    const carte = CarteEffetsFactory.generateCarte(effetsCartesJson);
 
-        expect(carte.effets[0]).toBeInstanceOf(DeplacementEffet);
-    });
+    expect(carte.effets[0]).toBeInstanceOf(DeplacementEffet);
+  });
 
-    test('retourne null pour un type inconnu', () => {
-        const effetsCartesJson = { 
-            type: 'type_inexistant', 
-            titre: 'X', 
-            description: 'Y'
-        };
+  test("retourne null pour un type inconnu", () => {
+    const effetsCartesJson = {
+      type: "type_inexistant",
+      titre: "X",
+      description: "Y",
+    };
 
-        const carte = CarteEffetsFactory.generateCarte(effetsCartesJson);
+    const carte = CarteEffetsFactory.generateCarte(effetsCartesJson);
 
-        expect(carte).toBeNull();
-    });
+    expect(carte).toBeNull();
+  });
 
- });
+  // tests parseCarteDeplacement()
+  test("parseCarteDeplacement() deplacement absolu", () => {
+    const jsonObj = {
+      titre: "Chance 1",
+      description: "Allez sur la case départ.",
+      type: TypesEffets.DEPLACEMENT,
+      type_deplacement: "absolu",
+      index_case: 2,
+      nombreDePas: 0,
+      bonusPassage: 200,
+    };
+
+    const carte = CarteEffetsFactory.parseCarteDeplacement(jsonObj);
+
+    expect(carte).toBeInstanceOf(CarteAction);
+    expect(carte.effets[0]).toBeInstanceOf(DeplacementEffet);
+    expect(carte.effets[0].typeDeplacement).toBe("absolu");
+    expect(carte.effets[0].valeurDeplacement).toBe(2); // index_case
+    expect(carte.effets[0].bonusDePassage).toBe(200);
+  });
+
+  test("parseCarteDeplacement() déplacement relatif (nombreDePas)", () => {
+    const json = {
+      titre: "Chance 2",
+      description: "Avancez de 3 cases",
+      type_deplacement: "relatif",
+      nombreDePas: 3,
+    };
+
+    const carte = CarteEffetsFactory.parseCarteDeplacement(json);
+
+    expect(carte.effets[0].valeurDeplacement).toBe(3);
+    expect(carte).toBeInstanceOf(CarteAction);
+    expect(carte.effets[0]).toBeInstanceOf(DeplacementEffet);
+  });
+
+  // tests parseCarteGareProche()
+  test("crée une carte avec un GareProcheEffet", () => {
+    const json = {
+      titre: "Chance 15",
+      description: "Allez à la gare la plus proche",
+    };
+
+    const carte = CarteEffetsFactory.parseCarteGareProche(json);
+
+    expect(carte.effets[0]).toBeInstanceOf(GareProcheEffet);
+  });
+
+  // tests parseCarteVersement()
+  test("crée un VersementEffet non collectif par défaut", () => {
+    const json = {
+      titre: "Chance 5",
+      description: "Payez 15 M",
+      montant: 15,
+      source: "joueur",
+      destinataire: "banque",
+    };
+
+    const carte = CarteEffetsFactory.parseCarteVersement(json);
+
+    expect(carte.effets[0]).toBeInstanceOf(VersementEffet);
+    expect(carte.effets[0].montant).toBe(15);
+    expect(carte.effets[0].estCollectif).toBe(false);
+  });
+
+  test('détecte "Fonds communs 9" comme versement collectif', () => {
+    const json = {
+      titre: "Fonds communs 9",
+      description: "Chaque joueur vous verse 10 M",
+      montant: 10,
+      source: "joueur",
+      destinataire: "joueur",
+    };
+
+    const carte = CarteEffetsFactory.parseCarteVersement(json);
+
+    expect(carte.effets[0].estCollectif).toBe(true);
+  });
+
+  // tests parseCarteAllerEnPrison()
+  test("crée un PrisonEffet(true)", () => {
+    const json = {
+      titre: "Chance 30",
+      description: "Allez en prison",
+    };
+
+    const carte = CarteEffetsFactory.parseCarteAllerEnPrison(json);
+
+    expect(carte.effets[0]).toBeInstanceOf(PrisonEffet);
+    expect(carte.effets[0].allerEnPrison).toBe(true);
+  });
+
+  // tests parseCarteSortirDePrison()
+  test("crée un PrisonEffet(false)", () => {
+    const json = {
+      titre: "Chance 9",
+      description: "Sortez de prison",
+    };
+
+    const carte = CarteEffetsFactory.parseCarteSortirDePrison(json);
+
+    expect(carte.effets[0]).toBeInstanceOf(PrisonEffet);
+    expect(carte.effets[0].allerEnPrison).toBe(false);
+  });
+
+  // tests parseCarteReparations()
+  test("crée un ReparationsEffet avec les bons montants", () => {
+    const json = {
+      titre: "Chance 23",
+      description: "Réparations",
+      montant_par_maison: 25,
+      montant_par_hotel: 100,
+      source: "joueur",
+      destinataire: "banque",
+    };
+
+    const carte = CarteEffetsFactory.parseCarteReparations(json);
+
+    expect(carte.effets[0]).toBeInstanceOf(ReparationsEffet);
+    expect(carte.effets[0].montant_par_maison).toBe(25);
+    expect(carte.effets[0].montant_par_hotel).toBe(100);
+  });
+
+  // tests parseCarteChoix()
+  test("crée un ChoixEffet", () => {
+    const json = {
+      titre: "Fonds communs 9",
+      description: "Choix",
+      montant: 10,
+      source: "joueur",
+      destinataire: "banque",
+    };
+
+    const carte = CarteEffetsFactory.parseCarteChoix(json);
+
+    expect(carte.effets[0]).toBeInstanceOf(ChoixEffet);
+  });
+
+  // tests melangerCartes()
+  test("conserve le même nombre de cartes", () => {
+    const cartes = [1, 2, 3, 4, 5];
+
+    const resultat = CarteEffetsFactory.melangerCartes(cartes);
+
+    expect(resultat.length).toBe(5);
+  });
+
+  test("contient toujours les mêmes éléments (juste réordonnés)", () => {
+    const cartes = ["c", "d", "a", "b"];
+
+    const resultat = CarteEffetsFactory.melangerCartes(cartes);
+
+    expect(resultat.sort()).toEqual(["a", "b", "c", "d"]);
+  });
+
+  test("mélange de façon prévisible quand Math.random est mocké", () => {
+    const cartes = [1, 2, 3];
+    jest.spyOn(Math, "random").mockReturnValue(0);
+
+    const resultat = CarteEffetsFactory.melangerCartes(cartes);
+
+    expect(resultat).toEqual([2, 3, 1]); // algo de melange Fisher-Yates avec Math.random() = 0
+    jest.restoreAllMocks(); // rétablit le comportement normal de Math.random() après le test
+  });
+});
